6c3d0fe50d993ec1160a0fd40f735dbd47cff322
Ryan Hellyer <ryanhellyer@gmail.com>
2016-08-23 19:25:11 +0200

    Syncing with dot org repo

--- disable-emojis.php
+++ disable-emojis.php
@@ -3,7 +3,7 @@
 Plugin Name: Disable Emojis
 Plugin URI: https://geek.hellyer.kiwi/plugins/disable-emojis/
 Description: Disable Emojis
-Version: 1.0
+Version: 1.5.1
 Author: Ryan Hellyer
 Author URI: https://geek.hellyer.kiwi/
 License: GPL2
@@ -29,14 +29,31 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 
 /**
- * Dequeue the Emoji script.
+ * Disable the emoji's
  */
-function disable_emoji_dequeue_script() {
-	wp_dequeue_script( 'emoji' );
+function disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );	
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
 }
-add_action( 'wp_print_scripts', 'disable_emoji_dequeue_script', 100 );
+add_action( 'init', 'disable_emojis' );
 
 /**
- * Remove the emoji styles.
+ * Filter function used to remove the tinymce emoji plugin.
+ * 
+ * @param    array  $plugins  
+ * @return   array             Difference betwen the two arrays
  */
-remove_action( 'wp_print_styles', 'print_emoji_styles' );
+function disable_emojis_tinymce( $plugins ) {
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	} else {
+		return array();
+	}
+}
+
